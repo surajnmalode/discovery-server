6f8c57a29e4f1b65a5e3292f7a26f0574f5b8171
surajnmalode <surajnmalode@gmail.com>
2020-05-11 20:26:50 +0530

    Code committed and pushed by surajnmalode@gmail.com on 11-May-2020 08:22 PM using DDLAB Gitpusher tool

---
+++ src/test/java/io/brains/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.brains.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
